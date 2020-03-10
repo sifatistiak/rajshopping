b2db88a06469567e8bb66ef4199b079f32dec15d
sifat <sifatistiak@gmail.com>
2020-03-11 03:47:53 +0600

    DELIVERY & RETURNS Sections

--- resources/views/includes/policy.blade.php
+++ resources/views/includes/policy.blade.php
@@ -18,14 +18,45 @@
 				<p>২| আমরা সাধারণত ঢাকার মধ্যে 24-48 ঘন্টা এবং ঢাকার বাহিরে 48-72 ঘন্টার মধ্যে ডেলিভারি করে থাকি | অনিবার্য কারনে পন্যের ডেলিভারিতে প্রতিশ্রুত ডেলিভারী
 				সময়ের চেয়ে বেশী সময় লাগতে পারে।
 				</p>
-				
+
 				<p>৩। প্রোডাক্ট হাতে পাওয়ার পর ডেলিভারি কর্মীকে টাকা বুঝিয়ে দিন।</p>
-				
+
 				<p>৪| প্রোডাক্টে কোন প্রকার ক্রটি থাকলে অথবা গুণগত মান নিয়ে কোন অভিযোগ থাকলে 24 ঘন্টার মধ্যে আমাদের সাথে যোগাযোগ করুন। যোগাযোগের নম্বর - <a>+880 1833996321</a>, ইমেইল - <a>support@kaporbd.com</a> অথবা ভিজিট করুন <a class="btn btn-link" href="{{route('quick.contact')}}">Quick Contact</a> </p>
-				
+
 			</div>
 
-		</div>
+        </div>
+
+        <div  class="row">
+            <!-- section title -->
+                        <div class="col-md-12">
+                            <div class="section-title">
+                                <h2 class="title">DELIVERY & RETURNS</h2>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3 text-right">
+                            <img class="col-md-4" src="{{asset('frontend/img/car.png')}}" alt="">
+                            <h6>FREE SHIPPING</h6>
+                            <p>Free shipping only Bangladesh order above 2000 BDT</p>
+                        </div>
+                        <div class="col-md-3 text-right">
+                            <img class="col-md-4" src="{{asset('frontend/img/support.png')}}" alt="">
+                            <h6>SUPPORT 24/7</h6>
+                            <p> Contact us 24 hours a day, 7 days a week</p>
+                        </div>
+                        <div class="col-md-3 text-right">
+                            <img class="col-md-4" src="{{asset('frontend/img/return.png')}}" alt="">
+                            <h6>7 DAYS RETURN</h6>
+                            <p>Simply return it within 7 days for an exchange.</p>
+                        </div>
+                        <div class="col-md-3 text-right">
+                            <img class="col-md-4" src="{{asset('frontend/img/secure.png')}}" alt="">
+                            <h6>100% PAYMENT SECURE</h6>
+                            <p>We ensure secure payment with PEV</p>
+                        </div>
+                        <!-- section title -->
+        </div>
 		<!-- /row -->
 	</div>
 	<!-- /container -->
