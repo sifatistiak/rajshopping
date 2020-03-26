236853e177138e6a928f04ffab62daa89db11bdd
sifat <sifatistiak@gmail.com>
2020-03-27 05:08:04 +0600

    popup arrive 1st time

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -165,27 +165,35 @@
 
 <script>
     $(document).ready(function() {
-    var id = '#dialog';
-    var maskHeight = $(document).height();
-    var maskWidth = $(window).width();
-    $('#mask').css({'width':maskWidth,'height':maskHeight});
-    $('#mask').fadeIn(500);
-    $('#mask').fadeTo("slow",0.9);
-    var winH = $(window).height();
-    var winW = $(window).width();
-    $(id).css('top', winH/2-$(id).height()/2);
-    $(id).css('left', winW/2-$(id).width()/2);
-    $(id).fadeIn(2000);
-    $('.window .close').click(function (e) {
-    e.preventDefault();
-    $('#mask').hide();
-    $('.window').hide();
-    });
-    $('#mask').click(function () {
-    $(this).hide();
-    $('.window').hide();
-    });
+        if (sessionStorage.getItem("loaded") == "ok") {
+            $('#mask').hide();
+            $('.window').hide();
 
+        } else {
+            var id = '#dialog';
+                var maskHeight = $(document).height();
+                var maskWidth = $(window).width();
+                $('#mask').css({'width':maskWidth,'height':maskHeight});
+                $('#mask').fadeIn(500);
+                $('#mask').fadeTo("slow",0.9);
+                var winH = $(window).height();
+                var winW = $(window).width();
+                $(id).css('top', winH/2-$(id).height()/2);
+                $(id).css('left', winW/2-$(id).width()/2);
+                $(id).fadeIn(2000);
+                $('.window .close').click(function (e) {
+                e.preventDefault();
+                sessionStorage.setItem("loaded", "ok");
+                $('#mask').hide();
+                $('.window').hide();
+                });
+                $('#mask').click(function () {
+                sessionStorage.setItem("loaded", "ok");
+                $(this).hide();
+                $('.window').hide();
+                });
+
+        }
     });
 </script>
 @endsection
